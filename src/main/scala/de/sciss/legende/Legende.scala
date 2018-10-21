0e5e9509ae39498945910d9e3b17f56f1218daec
Hanns Holger Rutz <contact@sciss.de>
2018-10-22 01:11:05 +0200

    ...

--- src/main/scala/de/sciss/legende/Legende.scala
+++ src/main/scala/de/sciss/legende/Legende.scala
@@ -248,6 +248,9 @@ object Legende {
   def segModTemp(idx: String)(implicit config: Config): File =
     config.dataDir / s"alphaville-$idx-segmod-%d.aif"
 
+  def segModRsmpTemp(idx: String)(implicit config: Config): File =
+    config.dataDir / s"alphaville-$idx-segmodRsmp-%d.aif"
+
   def getAudioCue[S <: Sys[S]](f: File)(implicit tx: S#Tx, config: Config, workspace: Workspace[S]): AudioCue.Obj[S] = {
     val r = workspace.root
     val fAudio = mkObj[S, Folder](r, "audio", VERSION)(mkAudioFolder())
@@ -299,6 +302,10 @@ object Legende {
 
     tl.attr.put("bounce-file", StringObj.newVar[S]((config.baseDir / "audio_work" / "legende.aif").path))
 
+    val trackHeight = 4
+
+    val pMain = addDefaultGlobalProc(tl)
+
     def oneTwo(isOne: Boolean, timeOff: Long): Long = {
       val temp        = segModTemp(if (isOne) "1" else "2")
       val gain        = 1.0 / (if (isOne) 9.0 else 6.0)
@@ -307,11 +314,9 @@ object Legende {
       val numSide     = leftIter.size
       assert (numSide === rightIter.size)
       //    val numFull     = (numSide + 1) / 2
-      val pMain       = addDefaultGlobalProc(tl)
       val fdShortLen  = (TimeRef.SampleRate * 0.1).toLong
       val fdShortOut  = FadeSpec(fdShortLen, Curve.parametric(+1.763f))
       val fdShortIn   = FadeSpec(fdShortLen, Curve.parametric(-1.763f))
-      val trackHeight = 4
       val tlLen       = {
         val f   = formatTemplate(temp, 1)
         val cue = getAudioCue(f)
@@ -405,13 +410,129 @@ object Legende {
       loop(isOdd = true )
     }
 
-    def three(timeOff: Long): Unit = {
-      ???
+    def three(timeOff: Long): Long = {
+      val tempRsmp    = segModRsmpTemp("3")
+      val tempA       = segModTemp("3a")
+      val tempB       = segModTemp("3b")
+      val gainRsmp    = 1.0 / 12.0
+      val gainA       = 1.0 / 8.0
+      val gainB       = 1.0 / 6.0
+      val leftIter    = 40 to 1 by -2
+      val rightIter   = 39 to 1 by -2
+      val numSide     = leftIter.size
+
+      val fdShortLen  = (TimeRef.SampleRate * 0.1).toLong
+      val fdShortOut  = FadeSpec(fdShortLen, Curve.parametric(+1.763f))
+      val fdShortIn   = FadeSpec(fdShortLen, Curve.parametric(-1.763f))
+
+      val tlLen = (leftIter ++ rightIter).map { iter =>
+        val f         = formatTemplate(tempRsmp, iter)
+        val cue       = getAudioCue(f)
+        (cue.numFrames * TimeRef.SampleRate / cue.sampleRate + 0.5).toLong
+      } .max
+
+      def loop(isOdd: Boolean): Long = {
+        val it  = if (isOdd) rightIter else leftIter
+        val pan = if (isOdd) +1.0 else -1.0
+        val trackIdxOff = if (isOdd) numSide else 0
+
+        val stops = it.zipWithIndex.map { case (iter, idx) =>
+          val fRsmp       = formatTemplate(tempRsmp, iter)
+          val fA          = formatTemplate(tempA   , iter)
+          val fB          = formatTemplate(tempB   , iter)
+          val cueRsmp     = getAudioCue(fRsmp)
+          val cueA        = getAudioCue(fA)
+          val cueB        = getAudioCue(fB)
+          val trackIdx    = (idx + trackIdxOff) * trackHeight
+          val cueRsmpLen  = (cueRsmp.numFrames * TimeRef.SampleRate / cueRsmp.sampleRate + 0.5).toLong
+          val cueALen     = (cueA   .numFrames * TimeRef.SampleRate / cueA   .sampleRate + 0.5).toLong
+          val cueBLen     = (cueB   .numFrames * TimeRef.SampleRate / cueB   .sampleRate + 0.5).toLong
+
+          val panRsmp = {
+            val g = Grapheme[S]()
+            val curve = CurveObj.newVar[S](CurveObj.newConst(Curve.lin))
+            g.add(LongObj.newVar(0L         ), EnvSegment.Obj(DoubleObj.newVar(pan), curve))
+            g.add(LongObj.newVar(cueRsmpLen ), EnvSegment.Obj(DoubleObj.newVar(pan/2), curve))
+            g
+          }
+
+          val panA = {
+            val g = Grapheme[S]()
+            val curve = CurveObj.newVar[S](CurveObj.newConst(Curve.lin))
+            g.add(LongObj.newVar(0L     ), EnvSegment.Obj(DoubleObj.newVar(pan/2), curve))
+            g.add(LongObj.newVar(cueALen), EnvSegment.Obj(DoubleObj.newVar(pan/4), curve))
+            g
+          }
+
+          val panB = {
+            val g = Grapheme[S]()
+            val curve = CurveObj.newVar[S](CurveObj.newConst(Curve.lin))
+            g.add(LongObj.newVar(0L     ), EnvSegment.Obj(DoubleObj.newVar(pan/4), curve))
+            g.add(LongObj.newVar(cueALen), EnvSegment.Obj(DoubleObj.newVar(0.0  ), curve))
+            g
+          }
+
+          def isMuted: Boolean = false
+
+//          def isMuted(j: Int): Boolean = j >= 0 && j <= numSide && {
+//            if (isOne) {
+//              if (!isOdd) {
+//                j > idx+1 && j < numSide - idx
+//              } else {
+//                j >= ((idx + 1) * 3)/2+1 && j <= numSide - (15 - idx)/2
+//              }
+//            } else {
+//              if (!isOdd) {
+//                j >= ((idx + 1) * 3)/2+1 && j < (numSide - idx/2)
+//              } else {
+//                j > idx+1 && j < numSide - idx
+//              }
+//            }
+//          }
+
+          val gOffset   = 0L
+          val mute      = isMuted
+          val fadeIn    = /* if (!mute && (i == idx || isMuted(i - 1))) fdMedIn  else */ fdShortIn
+          val fadeOut   = /* if (!mute &&              isMuted(i + 1))  fdMedOut else */ fdShortOut
+          val startRsmp = timeOff + tlLen - cueRsmpLen
+          val timeRsmp  = Span(startRsmp, startRsmp + cueRsmpLen)
+          val (_, pRsmp) = mkAudioRegion(tl, time = timeRsmp, audioCue = cueRsmp, pMain = pMain, gOffset = gOffset, gain = gainRsmp,
+            mute = mute, fadeIn = fadeIn, fadeOut = fadeOut, pan = pan,
+            trackIdx = trackIdx, trackHeight = trackHeight)
+//            if (i == numSide - 1) {
+//              p.attr.put("pan", penUltimatePan)
+//            } else if (i == numSide) {
+//              p.attr.put("pan", ultimatePan)
+//            }
+          pRsmp.attr.put("pan", panRsmp)
+
+          val startA  = timeRsmp.stop - fdShortLen
+          val timeA   = Span(startA, startA + cueALen)
+          val (_, pA) = mkAudioRegion(tl, time = timeA, audioCue = cueA, pMain = pMain, gOffset = gOffset, gain = gainA,
+            mute = mute, fadeIn = fadeIn, fadeOut = fadeOut, pan = pan,
+            trackIdx = trackIdx, trackHeight = trackHeight)
+          pA.attr.put("pan", panA)
+
+          val startB  = timeA.stop - fdShortLen
+          val timeB   = Span(startB, startB + cueBLen)
+          val (_, pB) = mkAudioRegion(tl, time = timeB, audioCue = cueB, pMain = pMain, gOffset = gOffset, gain = gainB,
+            mute = mute, fadeIn = fadeIn, fadeOut = fadeOut, pan = pan,
+            trackIdx = trackIdx, trackHeight = trackHeight)
+          pB.attr.put("pan", panB)
+
+          timeB.stop
+        }
+
+        stops.max
+      }
+
+      loop(isOdd = false)
+      loop(isOdd = true )
     }
 
     val stopOne = oneTwo(isOne = true , timeOff = 0L)
-    val stopTwo = oneTwo(isOne = false, timeOff = (stopOne + TimeRef.SampleRate * 0.3).toLong)
-    three(timeOff = (stopTwo + TimeRef.SampleRate * 1.2).toLong)
+    val stopTwo = oneTwo(isOne = false, timeOff = (stopOne + TimeRef.SampleRate * 1.3).toLong)
+    three(timeOff = (stopTwo + TimeRef.SampleRate * 1.8).toLong)
 
     tl
   }
