e53db8003c03cbe8c74da361ef7c57348dfc8538
Hanns Holger Rutz <contact@sciss.de>
2018-10-21 22:11:59 +0200

    can't use sinc interp

--- src/main/scala/de/sciss/legende/ResamplePaths.scala
+++ src/main/scala/de/sciss/legende/ResamplePaths.scala
@@ -16,26 +16,33 @@ package de.sciss.legende
 import de.sciss.file._
 import de.sciss.fscape.{GE, Graph, graph, stream}
 import de.sciss.kollflitz.Ops._
-import de.sciss.legende.Prepare.{readDijkstra, writeDijkstra, shouldWrite}
+import de.sciss.legende.Prepare.{readDijkstra, shouldWrite, writeDijkstra}
+import de.sciss.numbers.Implicits._
 import de.sciss.synth.io.{AudioFile, AudioFileSpec}
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.Duration
 
 object ResamplePaths {
-  final case class Config()
+  final case class Config(stage         : Int     = 3,
+                          iterations    : Int     = 40,
+                          waveformAmp0  : Double  = 1.0,
+                          waveformDamp  : Double  = 0.9,
+                         )
 
   def main(args: Array[String]): Unit = {
     // ---- make sure merged routes exist ----
 
     implicit val c: Config = Config()
+    import c._
     val baseDir   = file("/data/projects/Segmod")
     val dataDir   = baseDir / "data"
     val audioDir  = baseDir / "audio_work"
-    val stage     = 3
     val fSoundIn  = audioDir / s"AlphavilleTropComplexe-$stage.aif"
     val fSoundInA = audioDir / s"AlphavilleTropComplexe-${stage}a.aif"
     val fSoundInB = audioDir / s"AlphavilleTropComplexe-${stage}b.aif"
-    for (iter <- 1 to 40) {
+
+    for (iter <- 1 to iterations) {
       val fRouteA   = dataDir / s"alphaville-${stage}a-route-$iter.bin"
       val fRouteB   = dataDir / s"alphaville-${stage}b-route-$iter.bin"
       val fRouteOut = dataDir / s"alphaville-$stage-route-$iter.bin"
@@ -47,6 +54,16 @@ object ResamplePaths {
     }
 
     // ---- ----
+    for (iter <- 1 to iterations) {
+      val fRoute      = dataDir / s"alphaville-$stage-route-$iter.bin"
+      val fSegModRvs  = dataDir / s"alphaville-$stage-segmodRsmp-$iter.aif"
+      val waveformAmp = waveformAmp0 * waveformDamp.pow(iter - 1)
+
+      if (shouldWrite(fSegModRvs)) {
+        val fut = mkSegMod(fSoundIn = fSoundIn, fRoute = fRoute, fSegModRvs = fSegModRvs, outGain = waveformAmp)
+        Await.result(fut, Duration.Inf)
+      }
+    }
   }
 
   def any2stringadd: Any = ()
@@ -88,22 +105,14 @@ object ResamplePaths {
     writeDijkstra(fRouteOut, routeOut)
   }
 
-  def mkSegMod(fSoundIn: File, fSoundInA: File, fSoundInB: File, outGain: Double, fRouteA: File, fRouteB: File, fSegModRvs: File)
+  def mkSegMod(fSoundIn: File, outGain: Double, fRoute: File, fSegModRvs: File)
               (implicit config: Config): Future[Unit] = {
-    var routeA0     = readDijkstra(fRoute = fRouteA)
-    var routeB0     = readDijkstra(fRoute = fRouteB)
+    val route0  = readDijkstra(fRoute = fRoute)
 //    println(s"Route.length = ${route0.length}")
 //    if (route0.length < 100) println(route0.mkString("Vector(", ", ", ")"))
     val specIn      = AudioFile.readSpec(fSoundIn)
-    val specInA     = AudioFile.readSpec(fSoundInA)
-    val specInB     = AudioFile.readSpec(fSoundInB)
-    val overlapFrames = (specInB.numFrames + specInA.numFrames - specIn.numFrames).toInt
-    val overlapH    = overlapFrames/2
-
     val numFramesIn = specIn.numFrames.toInt
-
-
-    val route: Array[Int] = ???
+    val route: Array[Int] = route0
 
     import specIn.sampleRate
     val g = Graph {
@@ -112,12 +121,15 @@ object ResamplePaths {
       val periodsSz = periods.size
 //      println("PERIODS:")
 //      println(periods.take(50))
-      val freqN0    = ValueDoubleSeq(periods.map(1.0 / _): _*)
+      def freqN0    = ValueDoubleSeq(periods.map(1.0 / _): _*)
       val minFactor = 1.0 / 32
       val lineLen   = periodsSz * 10.0 // 2.0 // 1.9393   // Huh?
       val factor    = DC(1.0).take(periodsSz) ++ Line(0.0, 1.0, length = lineLen).linLin(0.0, 1.0, 1.0, minFactor).pow(10)
       val freqN     = Resample(in = freqN0 ++ freqN0 ++ freqN0, factor = factor, minFactor = 0.25,
         rollOff = 0.98, kaiserBeta = 6.0, zeroCrossings = 32).drop(periodsSz).take(lineLen)
+
+      RepeatWindow(freqN).poll(Metro(2), "freqN")
+      Length(freqN).poll(0, "num-freq")
 
       val phase0  = 0.25: GE
       val sh      = SegModPhasor(freqN, phase = 0.0 /* phase */) // .take(numFramesIn) // 0.25
@@ -129,8 +141,8 @@ object ResamplePaths {
       //    val sig     = ((sh + 0.5) % 1.0) * 2 - 1 // sawtooth (3)
       //    val sig     = sh * DC(0.0) // silence
       val specOut     = AudioFileSpec(numChannels = 1, sampleRate = sampleRate)
-      val framesDir   = AudioFileOut(sigDir, fSegModRvs, specOut)
-      Progress(framesDir / numFramesIn, Metro(sampleRate))
+//      val framesDir   = AudioFileOut(sigDir, fSegModRvs, specOut)
+//      Progress(framesDir / numFramesIn, Metro(sampleRate))
     }
 
     val cfg = stream.Control.Config()
